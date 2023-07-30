abd0da0289cd1e35bc95aff9d083fcab1740fa0b
ikenaga530 <takumi.ne.jp530@gmail.com>
2023-07-31 02:54:07 +0900

    [add] async C&R

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 import api.schemas.task as task_schema
 import api.cruds.task as task_crud
 from api.db import get_db
@@ -7,14 +8,22 @@ from api.db import get_db
 router = APIRouter()
 
 
+# @router.get("/tasks", response_model=list[task_schema.Task])
+# async def list_tasks(db: Session = Depends(get_db)):
+#     return task_crud.get_tasks_with_done(db)
+
 @router.get("/tasks", response_model=list[task_schema.Task])
-async def list_tasks(db: Session = Depends(get_db)):
-    return task_crud.get_tasks_with_done(db)
+async def list_tasks(db: AsyncSession = Depends(get_db)):
+    return await task_crud.get_tasks_with_done(db)
+
+# @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
+# async def create_task(task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
+#     return task_crud.create_task(db, task_body)
 
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
-    return task_crud.create_task(db, task_body)
+async def create_task(task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)):
+    return await task_crud.create_task(db, task_body)
 
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
